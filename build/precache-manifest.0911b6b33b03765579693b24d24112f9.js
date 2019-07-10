df98cd2e9c9d81a173478a1c392a139f24fc53b9
Alejandro Díaz Vecchio <aldiazve@gmail.com>
2019-07-10 08:02:34 -0500

    added github page

---
+++ build/precache-manifest.0911b6b33b03765579693b24d24112f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485d05019dcd4150184c9b68db60cd99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c92361123540b17ad5f3",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e19c76a05bd2bf28d6e5",
+    "url": "./static/js/2.f0780b97.chunk.js"
+  },
+  {
+    "revision": "c92361123540b17ad5f3",
+    "url": "./static/js/main.b516a92a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
